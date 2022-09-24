0151b05256f719cad763655ffc98349c59cbcc2c
galib hazarika <hazarika.galib@gmail.com>
2022-09-24 22:54:35 +0530

    Concept of hoisting is added

---
+++ variables/hoisting.js
@@ -0,0 +1,22 @@
+//Even before intialize we can acees the function and variable
+//Demo
+//This phenomena happen because on exectution of prog the global execution context comes into play
+//There first in memory black in from of key value pair the variables and function values are mapped
+//for variable the value at first phase is undefined and for functions it's function body
+//arrow function behave like variables not like function
+getName();
+console.log(weiredBehaviour); //give error as its act like variables
+weiredBehaviour2(); //give error as its act like variables
+console.log(x); //gives undefined
+console.log(getName);
+function getName() {
+  console.log("Hello World!");
+}
+let weiredBehaviour = () => {
+  console.log(2 + 3);
+};
+let weiredBehaviour2 = function () {
+  console.log("lion");
+};
+var x = 32;
+console.log(getName);
